82a278da79359a0b3c8ed63a250addc13701057d
Breninho23 <60906215+Breninho23@users.noreply.github.com>
2021-05-20 14:39:37 -0300

    Update main.nim
    Inserção do SUBMENU

--- main.nim
+++ main.nim
@@ -1,26 +1,36 @@
 import utils
-import strformat
+import objs
 import strutils
+import strformat
 var cli=Cliente
 var fun=Funcionario
 var forn=Fornecedor
 var pro=Produto
-var x = "9"
-while x != "10":
+var x = "1"
+while x != "7":
+
     menu()
-    var x = readline(stdin)
-    
+    var x = readline(stdin)    
     case x
-    of "1": cadfuncionario() insertFuncionario(fun)
-    of "2": cadCliente() insertCliente(cli)
-    of "3": cadProduto() insertEstoque(pro)
-    of "4": cadfornecedor() insertFornecedor(forn)
-    of "5": 
-    of "6": 
-    of "7": 
-    of "8":
-    of "9": 
-    of "10": 
-        echo "Obrigado por utilizar o nosso sistema"
-        break
-    else: echo "Opção inválida!, tente novamente"
+        of "1": cadfuncionario() insertFuncionario(fun)
+        of "2": cadCliente() insertCliente(cli)
+        of "3": cadProduto() insertEstoque(pro)
+        of "4": cadfornecedor() insertFornecedor(forn)
+        of "5":
+        of "6": echo "Obrigado por utilizar o nosso sistema"
+        else: echo "Opção inválida!, tente novamente"
+        
+    var y = readline(stdin) 
+    case y
+      of "1": listClientes()
+      of "2": listEstoque()
+      of "3": listFuncionario()
+      of "4": listFornecedores()
+      of "5": vendas()
+      of "6": echo "<<<<<<<<"   
+      else: echo "Opção inválida" 
+    
+   
+        
+        
+    
